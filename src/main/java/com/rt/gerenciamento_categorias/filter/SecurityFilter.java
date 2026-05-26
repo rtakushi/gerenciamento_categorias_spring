c1b0561416726deab8c1729a9957ad327f35b13c
unknown <takushirenan@gmail.com>
2026-05-25 22:07:51 -0300

    security filter criado

---
+++ src/main/java/com/rt/gerenciamento_categorias/filter/SecurityFilter.java
@@ -0,0 +1,25 @@
+package com.rt.gerenciamento_categorias.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement( session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.GET, "/categorias").permitAll())
+                .build();
+    }
+
+}
